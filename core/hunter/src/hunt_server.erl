61826894d8786c1aec91756b98e9184423592452
chenjie <chenjie7@le.com>
2017-01-21 00:51:09 +0800

    add httpc request.

--- core/hunter/src/hunt_server.erl
+++ core/hunter/src/hunt_server.erl
@@ -7,10 +7,11 @@
 		 start/1,
 		 init/1,
 		 handle_call/3,
-		 handle_cast/2]).
+		 handle_cast/2,
+		 handle_info/2]).
 
 -define(SERVER, ?MODULE).
--record(user, {name, intro, updated}).
+-record(user, {name, intro, reqid, updated}).
 
 start_link() ->
 	start_link(default_tag).
@@ -40,7 +41,8 @@ list(Pid) ->
 	gen_server:call(Pid, list).
 
 current() ->
-	calendar:now_to_local_time(erlang:timestamp()).
+	{{Y, M, D}, {H, Mi, S}}= calendar:now_to_local_time(erlang:timestamp()),
+	io_lib:format("~4..0b-~2..0b-~2..0b, ~2..0b:~2..0b:~2..0b", [Y,M,D,H,Mi,S]).
 
 format_users(L) -> format_users(L, []).
 
@@ -53,23 +55,25 @@ format_users([], Acc) ->
 handle_cast({hunt, Name}, State1) ->
 	{Tag, Users1} = State1,
 	CurrentTime = current(),
+	{ok, RequestId} = httpc:request(get, {"http://localhost:8000", []}, [], [{sync, false}]),
+
 	Users2 = case lists:keyfind(Name, #user.name, Users1) of
 		#user{} ->
 			% found
-			User = #user{name=Name, intro=none, updated=CurrentTime},
+			User = #user{name=Name, reqid=RequestId,
+						 intro=none, updated=CurrentTime},
 			lists:keyreplace(Name, #user.name, Users1, User);
 		false ->
-			User = #user{name=Name, intro=none, updated=CurrentTime},
+			User = #user{name=Name, reqid=RequestId,
+						 intro=none, updated=CurrentTime},
 			[User | Users1]
 	end,
 	State2 = {Tag, Users2},
-	io:format("State is: ~p~n", [State2]),
 	{noreply, State2}.
 
 %% first in first out.
 handle_call({show, Name}, _From, State) ->
 	{_, Users} = State,
-	io:format("users: ~p, name: ~p ~n", [Users, Name]),
 	Info = case lists:keyfind(Name, #user.name, Users) of
 		#user{intro=Intro, updated=Updated} ->
 			{Intro, Updated};
@@ -82,3 +86,18 @@ handle_call(list, _From, State) ->
 	{_Privacy, Users} = State,
 	Formatted = format_users(Users),
 	{reply, Formatted, State}.
+
+handle_info({http, {RequestId, Result}}, State1) ->
+	{Tag, Users1} = State1,
+	CurrentTime = current(),
+	Users2 = case lists:keyfind(RequestId, #user.reqid, Users1) of
+		#user{name=Name, reqid=RequestId} ->
+			% found
+			User = #user{name=Name, reqid=RequestId, intro=Result, updated=CurrentTime},
+			lists:keyreplace(RequestId, #user.reqid, Users1, User);
+		false ->
+			io:format("not found request in previouse user lists??"),
+			Users1
+	end,
+	State2 = {Tag, Users2},
+	{noreply, State2}.
